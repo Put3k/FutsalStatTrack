950d5f62f2d0cdcf4a80d0e26d3f1f06aa071e32
Kacper Winiarczyk <114064711+Put3k@users.noreply.github.com>
2023-08-21 17:36:56 +0200

    Save production settings and docker-compose.yml file

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-from decouple import config
 from environs import Env
 from google.oauth2 import service_account
 
@@ -27,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
@@ -209,4 +208,4 @@ INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
 GS_CREDENTIALS = service_account.Credentials.from_service_account_file(os.path.join(BASE_DIR, 'credential.json'))
 
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-GS_BUCKET_NAME = config("GS_BUCKET_NAME")
+GS_BUCKET_NAME = env("GS_BUCKET_NAME")
