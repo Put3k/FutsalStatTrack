93b7e880eb3556e4a00cf8340bd7b4a2e52315c6
Put3k <fightepucio@gmail.com>
2023-11-09 19:56:22 +0100

    Change metodology in calculating player points on stat_track admin actions

--- stat_track/actions.py
+++ stat_track/actions.py
@@ -16,22 +16,21 @@ def summarize_player_stats(modeladmin, request, queryset):
     with transaction.atomic():
 
         for player in queryset:
-            if not player.playerstatsum_set.exists():
-                for league in player.leagues.all():
-                    existing_player_stat_sum = player.playerstatsum_set.filter(league=league).first()
+            for league in player.leagues.all():
+                existing_player_stat_sum = player.playerstatsum_set.filter(league=league).first()
 
-                    if not existing_player_stat_sum:
-                        player_stat_sum = PlayerStatSum(player=player, league=league)
-                    else:
-                        player_stat_sum = existing_player_stat_sum
+                if not existing_player_stat_sum:
+                    player_stat_sum = PlayerStatSum(player=player, league=league)
+                else:
+                    player_stat_sum = existing_player_stat_sum
 
-                    player_stat_sum.goals = player.stat_set.filter(league=league).aggregate(Sum('goals'))['goals__sum'] or 0
-                    player_stat_sum.match_count = player.stat_set.filter(league=league).count()
-                    player_stat_sum.matchday_count = player.matchdayticket_set.filter(matchday__league=league).count()
-                    player_stat_sum.wins = sum(1 for stat in player.stat_set.filter(league=league) if stat.win is True)
-                    player_stat_sum.draws = sum(1 for stat in player.stat_set.filter(league=league) if stat.win == "Draw")
-                    player_stat_sum.loses = sum(1 for stat in player.stat_set.filter(league=league) if stat.win is False)
-                    player_stat_sum.points = 3 * player_stat_sum.wins + player_stat_sum.draws
-                    player_stat_sum.save()
+                player_stat_sum.goals = player.stat_set.filter(league=league).aggregate(Sum('goals'))['goals__sum'] or 0
+                player_stat_sum.match_count = player.stat_set.filter(league=league).count()
+                player_stat_sum.matchday_count = player.matchdayticket_set.filter(matchday__league=league).count()
+                player_stat_sum.wins = sum(1 for stat in player.stat_set.filter(league=league) if stat.win is True)
+                player_stat_sum.draws = sum(1 for stat in player.stat_set.filter(league=league) if stat.win == "Draw")
+                player_stat_sum.loses = sum(1 for stat in player.stat_set.filter(league=league) if stat.win is False)
+                player_stat_sum.points = 3 * player_stat_sum.wins + player_stat_sum.draws + player_stat_sum.goals * 0.5
+                player_stat_sum.save()
 
         # Populate existing PlayerStatSum instances
